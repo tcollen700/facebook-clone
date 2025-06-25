dd59bada3984eb4a5b32f95d1347416ca2e5b47d
Tshepiso Collen Mkhwanazi <42327513+tcollen700@users.noreply.github.com>
2025-06-25 04:51:06 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
+const postTextElements = document.querySelectorAll('.image-post-text');
 
+postTextElements.forEach(postText => {
+    postText.addEventListener('click', () => {
+        if (postText.classList.contains("image-post-text")) {
+            postText.classList.add("post-text");
+            postText.classList.remove("image-post-text");
+        } else {
+            postText.classList.add("image-post-text");
+            postText.classList.remove("post-text");
+        }
+    });
+});
+
+/**let postText = document.querySelectorAll('.image-post-text');
+
+postText.addEventListener('click', () => {
+    if (postText.classList.contains("image-post-text")) {
+        postText.classList.add("post-text");
+        postText.classList.remove("image-post-text");
+    } else {
+        postText.classList.add("image-post-text");
+        postText.classList.remove("post-text");
+    }
+});**/
